4fea3fd9ff644601b3a4be7cb0cf37521a387267
Avinash <kirangp@gmail.com>
2017-04-22 09:23:48 +0000

    Changed config and tested functionality so that Vuejs works with Phoenix web app

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,21 +1,15 @@
-// Brunch automatically concatenates all files in your
-// watched paths. Those paths can be configured at
-// config.paths.watched in "brunch-config.js".
-//
-// However, those files will only be executed if
-// explicitly imported. The only exception are files
-// in vendor, which are never wrapped in imports and
-// therefore are always executed.
-
-// Import dependencies
-//
-// If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
 import "phoenix_html"
 
-// Import local files
-//
-// Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
+import Vue from 'vue'
+import MyApp from "../components/my-app.vue"
 
-// import socket from "./socket"
+// Create the main component
+Vue.component('my-app', MyApp)
+
+// And create the top-level view model:
+new Vue({
+  el: '#app',
+  render(createElement) {
+    return createElement(MyApp, {})
+  }
+});

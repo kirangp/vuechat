f780828cad6ccc00e4669e4fc2f4f320e72b5ce2
Avinash <kirangp@gmail.com>
2017-04-22 11:13:09 +0000

    Chat system working

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,15 +1,2 @@
 import "phoenix_html"
-
-import Vue from 'vue'
-import MyApp from "../components/my-app.vue"
-
-// Create the main component
-Vue.component('my-app', MyApp)
-
-// And create the top-level view model:
-new Vue({
-  el: '#app',
-  render(createElement) {
-    return createElement(MyApp, {})
-  }
-});
+import socket from "../js/socket"
